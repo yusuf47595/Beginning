0d25f909026b92557d621d54eba6a826e73f5b3d
yusufk3579 <yusufkhan3579@gmail.com>
2020-07-31 22:13:57 +0530

    driver addition

--- src/test/java/Base.java
+++ src/test/java/Base.java
@@ -1,4 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
+@Test
 public class Base {
+	
+	public void driver() {
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.aptronixindia.com/");
 
+	}
+	
 }
